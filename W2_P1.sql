35cfbc76063ee97859dde82197a8f865290c483c
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2022-08-11 10:35:39 +0100

    adding week 2 practice 1 file
    this file focuses on using the INVERTED INDEX and seeing how it works

---
+++ W2_P1.sql
@@ -0,0 +1,23 @@
+DROP TABLE docs;
+CREATE TABLE docs (
+    id SERIAL PRIMARY KEY,
+    doc TEXT
+);
+
+-- creating gin index
+CREATE INDEX gin1 on docs using GIN(string_to_array(doc, ' ') array_ops);
+
+-- insert values into docs
+INSERT INTO docs (doc) VALUES
+('This is SQL and Python and other fun teaching stuff'),
+('More people should learn SQL from UMSI'),
+('UMSI also teaches Python and also SQL');
+
+-- adding extra values to make sure
+-- this is done to ensure that the inverted index is used
+INSERT INTO docs(doc) select 'NEON ' || generate_series(10000, 20000);
+
+-- wait a while for the index to be created
+select id, doc from docs where '{learn}' <@ string_to_array(doc, ' ');
+-- the "<@" sign means contains
+
